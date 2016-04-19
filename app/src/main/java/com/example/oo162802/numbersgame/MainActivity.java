c6bd91078f84cfc9a0f34ff7516f8458dadec016
dtytomi <dtytomi@gmail.com>
2016-04-19 17:16:08 +0100

    design almost done adding functionalty

--- app/src/main/java/com/example/oo162802/numbersgame/MainActivity.java
+++ app/src/main/java/com/example/oo162802/numbersgame/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.oo162802.numbersgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
+import android.widget.TableRow;
+import android.widget.TableLayout;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int[] symbol;
+    private int sum;
+
+    private EditText userEditText;
+    private TextView numberTextView1;
+    private TextView numberTextView2;
+    private TextView scoreTextView1;
+    private TextView symbolTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Random
+
     }
 }
